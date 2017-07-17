3d5c4aeb6a1ae83cec25c5bf1b083d7e4d3d85c7
Rick Henry <fredericmhenry@gmail.com>
2017-07-16 22:27:57 -0400

    translate to python3. move sensitive data out of source
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- get_mail.py
+++ get_mail.py
@@ -1,36 +1,46 @@
 import smtplib, time, imaplib, email
+import configparser
 
-ORG_EMAIL = "@gmail.com"
-FROM_EMAIL = "rickspamgarbage" + ORG_EMAIL
-FROM_PWD = "hffwtxcgovthgfin"
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT = 993
+
+def get_login():
+    config = configparser.ConfigParser()
+    config.read("./login.conf")
+
+    return (
+            config['login']['user'],
+            config['login']['password'],
+            config['server']['imap_url'],
+            int(config['server']['port'])
+            )
+
+FROM_EMAIL, FROM_PWD, SMTP_SERVER, SMTP_PORT = get_login()
 
 def readmail():
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL, FROM_PWD)
-        mail.select('inbox')
+    # try:
+    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+    mail.login(FROM_EMAIL, FROM_PWD)
+    mail.select('inbox')
 
-        type, data = mail.search(None, 'ALL')
-        mail_ids = data[0]
-        
-        id_list = mail_ids.split()
-        first_email_id = int(id_list[0])
-        latest_email_id = int(id_list[-1])
+    type, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
 
-        for i in range(latest_email_id,first_email_id, -1):
-            typ, data = mail.fetch(i, '(RFC822)')
-            
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(response_part[1])
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    print 'From: ' + email_from 
-                    print 'Subject: ' + email_subject 
-                    print 'To: ' + msg['to'] + '\n'
-    except Exception, e:
-        print str(e)
+    id_list = mail_ids.split()
+    first_email_id = int(id_list[0])
+    latest_email_id = int(id_list[-1])
+
+    for i in range(latest_email_id,first_email_id, -1):
+        typ, data = mail.fetch(str(i), '(RFC822)')
+
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_bytes(response_part[1])
+                email_subject = msg['subject']
+                email_from = msg['from']
+                print('From: ' + email_from)
+                print('Subject: ' + email_subject)
+                print('To: ' + msg['to'] + '\n')
+
+    # except Exception as e:
+    #     print(str(e))
 
 readmail()
