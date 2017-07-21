9195ab5665c5a0a5e0398f57a8f45c5937a9355d
Rick Henry <fredericmhenry@gmail.com>
2017-07-20 22:32:24 -0400

    move some files
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup,find_packages
+from codecs import open
+from os import path
+from subprocess import run
+
+here = path.abspath(path.dirname(__file__))
+vers = '0.1.0'
+desc = 'Script to create task records in airtable from parsed emails'
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name='attaskcreator',
+        version=vers,
+
+        description=desc,
+        long_description=long_description,
+
+        url='https://github.com/rickh94/attaskcreator.git',
+        author='Rick Henry',
+        author_email='fredericmhenry@gmail.com',
+
+        license='GPLv3',
+        python_requires='>=3',
+
+        packages=find_packages(),
+
+        entry_points={
+            'console_scripts': [
+                'attaskcreator = attaskcreator.main:main',
+                ],
+            },
+        )
