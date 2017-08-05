609114e03e2ca1fe0c8c72a90ebae86bed200158
Rick Henry <fredericmhenry@gmail.com>
2017-08-04 20:12:55 -0400

    fix smtplib problem
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- tests/test_email_methods.py
+++ tests/test_email_methods.py
@@ -28,13 +28,15 @@ class TestEmailMethods(unittest.TestCase):
             "Bill Clinton <bclinton@whitehouse.gov>",
             ]
         self.recip_dicts = [
-            {'fname': 'John', 'lname': 'Smith', 'email': 'johnsmith@example.com'},
-            {'fname': 'Barack', 'lname': 'Obama', 'email': 'bobama@whitehouse.gov'},
+            {'fname': 'John', 'lname': 'Smith', 'email':
+             'johnsmith@example.com'},
+            {'fname': 'Barack', 'lname': 'Obama', 'email':
+             'bobama@whitehouse.gov'},
             {'fname': 'Cory', 'lname': 'Booker', 'email': 'cbooker@senate.gov'},
             {'fname': '', 'lname': '', 'email': 'admin@google.com'},
-            {'fname': 'Bill', 'lname': 'Clinton', 'email': 'bclinton@whitehouse.gov'},
+            {'fname': 'Bill', 'lname': 'Clinton', 'email':
+             'bclinton@whitehouse.gov'},
             ]
-
 
     @mock.patch.object(retrievemail.FetchMail, 'select')
     @mock.patch.object(retrievemail.FetchMail, 'login')
@@ -44,14 +46,14 @@ class TestEmailMethods(unittest.TestCase):
         mock_login.assert_called_once_with('testuser', 'password')
         mock_select.assert_called_once_with('Inbox')
 
-
     @mock.patch.object(retrievemail.email, 'message_from_bytes')
     @mock.patch.object(retrievemail.FetchMail, 'close')
     @mock.patch.object(retrievemail.FetchMail, 'fetch')
     @mock.patch.object(retrievemail.FetchMail, 'search')
     @mock.patch.object(retrievemail.FetchMail, 'select_inbox')
     def test_fetch_unread_messages(self, mock_select_inbox, mock_search,
-                                   mock_fetch, mock_close, mock_message_from_bytes):
+                                   mock_fetch, mock_close,
+                                   mock_message_from_bytes):
         """Test fetching unread messages"""
         # return one message
         mock_search.return_value = ('OK', [b'1'])
@@ -65,7 +67,6 @@ class TestEmailMethods(unittest.TestCase):
         mock_search.assert_called_once_with(None, 'UnSeen')
         mock_fetch.assert_called_once_with(b'1', '(RFC822)')
         mock_message_from_bytes.assert_called_once_with('testmsg')
-
 
     def test_save_attach(self):
         """Test saving attachments from an email message."""
@@ -82,13 +83,11 @@ class TestEmailMethods(unittest.TestCase):
         m.assert_any_call(filepath2, 'wb')
         self.assertListEqual([filepath1, filepath2], paths)
 
-
     def test_get_msg_text(self):
         """Test getting message content."""
         self.assertRegex(
             retrievemail.get_msg_text(self.testmsg),
             r'.*?This is a test email\..*?')
-
 
     @mock.patch('attaskcreator.retrievemail.get_msg_text')
     @mock.patch('attaskcreator.retrievemail.html2text')
@@ -107,12 +106,11 @@ class TestEmailMethods(unittest.TestCase):
         mock_get_msg.assert_called_once_with(testmsg)
         assert mock_html2text.called
 
-
     def test_parse_to(self):
         """Test case for parse_to_field."""
-        self.assertListEqual(retrievemail.parse_to_field(', '.join(self.recips)),
+        self.assertListEqual(retrievemail.parse_to_field(
+            ', '.join(self.recips)),
                              self.recip_dicts)
-
 
     def test_parse_recip(self):
         """Test case for parse_recipient."""
@@ -121,8 +119,7 @@ class TestEmailMethods(unittest.TestCase):
                 self.assertDictEqual(retrievemail.parse_recipient(recip),
                                      self.recip_dicts[num])
 
-
-    @mock.patch('attaskcreator.retrievemail.smtplib.SMTP')
+    @mock.patch('smtplib.SMTP')
     def test_send_msg(self, mock_smtp):
         retrievemail.sendmsg(
             mock_smtp,
