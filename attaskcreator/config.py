a7df9d48311e0216db67377fd9ca91327bff72bb
Rick Henry <fredericmhenry@gmail.com>
2017-08-07 23:52:49 -0400

    change config to object
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- attaskcreator/config.py
+++ attaskcreator/config.py
@@ -3,91 +3,124 @@ import configparser
 import atexit
 import os
 import logging
+import daiquiri
 from attaskcreator import settings
 from attaskcreator.atinterface import MyDatabase
 from attaskcreator import exceptions
 
 
-def setattrs(_self, **kwargs):
-    """Quickly set multiple attributes from key value pairs)."""
-    for k, v in kwargs.items():
-        setattr(_self, k, v)
+class Settings(object):
+    """Sets the various configuration options for attaskcreator."""
+    def __init__(self):
+        """Read the configuration files."""
+        self.login = configparser.ConfigParser()
+        self.tables = configparser.ConfigParser()
+        try:
+            self.login.read("/etc/attaskcreator/login.conf")
+            self.tables.read("/etc/attaskcreator/tables.conf")
+            with open("/etc/attaskcreator/phrases.conf", "r") as f:
+                self.phrases = f.readlines()
+        except OSError:
+            logging.exception(
+                'Could not open config files. Traceback Follows:')
+            raise SystemExit(1)
+        self.logger = daiquiri.getLogger(__name__)
 
+    def setup_eml(self):
+        """Sets email options."""
+        try:
+            self.eml_username = self.login['Email']['user']
+            self.eml_pwd = self.login['Email']['password']
+            self.eml_imap_server = self.login['Email']['imap url']
+            self.eml_smtp_server = self.login['Email']['smtp url']
+            self.eml_error = self.login['Email']['error email']
+        except KeyError as err:
+            raise exceptions.ConfigError(
+                'Missing email config info: {}'.format(err))
 
-def get_settings():
-    """Reads /etc/attaskcreator/attaskcreator.conf to configure needed options.
+    def setup_db(self):
+        """Sets up the airtable database."""
+        if 'app' not in self.login['Airtable']['database id']:
+            raise exceptions.ConfigError("Database ID is not correct")
+        if 'key' not in login['Airtable']['api key']:
+            raise exceptions.ConfigError("API key is not correct")
+        try:
+            self.atdb = MyDatabase(
+                login['Airtable']['database id'],
+                login['Airtable']['api key']
+            )
+        except KeyError as err:
+            raise exceptions.ConfigError(
+                'Missing airtable config info: {}'.format(err))
 
-    Login options are stored, used to login, or exported to the environment.
-    """
-    login = configparser.ConfigParser()
-    tables = configparser.ConfigParser()
-    try:
-        login.read("/etc/attaskcreator/login.conf")
-        tables.read("/etc/attaskcreator/tables.conf")
-        with open("/etc/attaskcreator/phrases.conf", "r") as f:
-            phrases = f.readlines()
-    except OSError:
-        logging.exception('Could not open config files. Traceback Follows:')
-        raise SystemExit(8)
+    def setup_phrases(self):
+        """Sets up the trigger phrases and termination character for
+        attaskcreator.
+        """
+        self.phrases = list(map(lambda x: x.strip(), self.phrases))
+        self.term_char = self.tables['Parse']['termination character']
 
-    # strip whitespace and newlines
-    phrases = list(map(lambda x: x.strip(), phrases))
+    def setup_aws(self):
+        """Sets up AWS access info and bucket. If running hosted in AWS this
+        should be implemented using IAM and NOT an API key.
+        """
+        try:
+            os.environ['AWS_ACCESS_KEY_ID'] = (
+                self.login['AWS']['access key id'])
+            os.environ['AWS_SECRET_ACCESS_KEY'] = (
+                self.login['AWS']['secret access key'])
+        except KeyError:
+            error = exceptions.ConfigError("AWS access configuration missing")
+            try:
+                if self.login['AWS']['using iam'].lower() == 'true':
+                    pass
+                else:
+                    raise error
+            except KeyError:
+                raise error
+        else:
+            atexit.register(unset_aws)
+        try:
+            self.bucket = self.login['AWS']['bucket']
+        except KeyError:
+            raise exceptions.ConfigError("AWS bucket configuration missing.")
 
-    # make airtable object
-    if 'app' not in login['Airtable']['database id']:
-        raise exceptions.ConfigError("Database ID is not correct")
-    if 'key' not in login['Airtable']['api key']:
-        raise exceptions.ConfigError("API key is not correct")
+    def setup_tables(self):
+        """Setup the tables for record search and insertion in airtable."""
+        try:
+            # tasks table
+            self.at_tasks_table = self.tables['Tasks Table']['name']
+            self.tasks_table_person = (
+                self.tables['Tasks Table']['people link field'])
+            self.tasks_table_text = self.tables['Tasks Table']['text field']
+            self.tasks_table_attach = (
+                self.tables['Tasks Table']['attachment link field'])
 
-    atdb = MyDatabase(
-        login['Airtable']['database id'],
-        login['Airtable']['api key']
-    )
+            # people table
+            self.at_people_table = self.tables['People Table']['name']
+            self.people_table_key = self.tables['People Table']['email field']
 
-    setattrs(settings,
-             # email config
-             eml_username=login['Email']['user'],
-             eml_pwd=login['Email']['password'],
-             eml_imap_server=login['Email']['imap url'],
-             eml_smtp_server=login['Email']['smtp url'],
-             eml_error=login['Email']['error email'],
+            # files table
+            self.at_files_table = self.tables['Files Table']['name']
+            self.files_table_name_field = (
+                self.tables['Files Table']['key field'])
+            self.files_table_attach_field = (
+                 self.tables['Files Table']['Attachment Field'])
+        except KeyError as err:
+            raise exceptions.ConfigError(
+                "Table configuration missing: {}".format(err))
+        try:
+            self.tasks_table_notes = self.tables['Tasks Table']['notes field']
+        except KeyError:
+            pass
 
-             # database config
-             database=atdb,
-
-             # temporary bucket
-             bucket=login['AWS']['bucket'],
-
-             # tasks table
-             at_tasks_table=tables['Tasks Table']['name'],
-             tasks_table_person=tables['Tasks Table']['people link field'],
-             tasks_table_text=tables['Tasks Table']['text field'],
-             tasks_table_notes=tables['Tasks Table']['notes field'],
-             tasks_table_attach=tables['Tasks Table']['attachment link field'],
-
-             # people table
-             at_people_table=tables['People Table']['name'],
-             people_table_key=tables['People Table']['email field'],
-
-             # files table
-             at_files_table=tables['Files Table']['name'],
-             files_table_name_field=tables['Files Table']['key field'],
-             files_table_attach_field=(
-                 tables['Files Table']['Attachment Field']),
-
-             # text parsing config
-             trigger_phrases=phrases,
-             term_char=tables['Parse']['termination character'],
-             )
-
-    # set environment variables for aws
-    try:
-        os.environ['AWS_ACCESS_KEY_ID'] = login['AWS']['access key id']
-        os.environ['AWS_SECRET_ACCESS_KEY'] = login['AWS']['secret access key']
-    except KeyError:
-        pass
-    else:
-        atexit.register(unset_aws)
+    def setup_all():
+        """Sets up everything for attaskcreator.  """
+        self.setup_eml()
+        self.setup_db()
+        self.setup_phrases()
+        self.setup_aws()
+        self.setup_tables()
 
 
 def unset_aws():
