b2f941f31aa4c4ff9c1bb0d5f130aa5477906623
Rick Henry <fredericmhenry@gmail.com>
2017-08-18 01:56:03 -0400

    fix accidental uncomment
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- attaskcreator/main.py
+++ attaskcreator/main.py
@@ -13,7 +13,7 @@ from attaskcreator import exceptions
 app = flask.Flask(__name__)
 app.config.from_object(__name__)
 
-TODO: These objects need to move
+# TODO: These objects need to move
 slackin = Slack(app)
 mytoken = os.environ['SLACK_VER_TOKEN']
 myid = os.environ['TEAM_ID']
